8000fd4c1ee97e6005a1829a95dd12c7877e152e
Soham Kumbhar <92673331+Sohamm21@users.noreply.github.com>
2025-12-13 19:43:02 +0530

    Fix: Update header color to red and title to Soham's ToDo

--- src/pages/TodoWrapper.tsx
+++ src/pages/TodoWrapper.tsx
@@ -33,8 +33,8 @@ const TodoWrapper = () => {
 
   return (
     <div className="todo-wrapper px-2 sm:px-70">
-      <div className="header bg-linear-to-t from-sky-700/60 to-indigo-500/60 mb-5 sm:mb-10 rounded-b-xl">
-        <h1>Todo App</h1>
+      <div className="header bg-red-500 mb-5 sm:mb-10 rounded-b-xl">
+        <h1>Soham's ToDo</h1>
       </div>
       <div className="todo-list">
         <CurrentTasks tasks={tasks} setTasks={setTasks} />
